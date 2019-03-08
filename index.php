a9ac8c58b23edbef27d8ce850a46d2a4fdea2fc0
SaigyoujiYuyuko233 <purple_guy@qq.com>
2019-03-08 20:27:40 +0800

    add: 配置文件

--- index.php
+++ index.php
@@ -9,4 +9,9 @@
  * Copyright © 2019 SaigyoujiYuyuko. All rights reserved.
  */
 
+define("START_TIME",microtime(true));
+define("ROOT_DIR", str_replace("\\", "/", __DIR__));
+
+
 require_once "WanderFile/loader.php";
+require_once "WanderFile/config.php";
