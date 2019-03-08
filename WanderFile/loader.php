601612663ef090da4913c03a765731d74b362082
SaigyoujiYuyuko233 <purple_guy@qq.com>
2019-03-08 19:39:33 +0800

    init the files

---
+++ WanderFile/loader.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * ProjectName: WanderFileManager.
+ * Author: SaigyoujiYuyuko
+ * QQ: 3558168775
+ * Date: 2019/3/8
+ * Time: 19:34
+ *
+ * Copyright © 2019 SaigyoujiYuyuko. All rights reserved.
+ */
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * ProjectName: PowerFileManager.
+ * Author: SaigyoujiYuyuko
+ * QQ: 3558168775
+ * Date: 2019/3/8
+ * Time: 19:27
+ *
+ * Copyright © 2019 SaigyoujiYuyuko. All rights reserved.
+ */
+
+require_once "WanderFile/loader.php";
